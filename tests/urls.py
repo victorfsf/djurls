d6e61e1529e6d8c7590dbf243a5cdaf2c64b2175
Victor Ferraz <victorfsf.dev@gmail.com>
2016-05-29 16:58:31 -0300

    Update urls.py

--- tests/urls.py
+++ tests/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 
 mock_patterns = []
 included_patterns = []
+
 urlpatterns = [
     url('^mock/', include(mock_patterns, namespace='mock')),
     url('^included/', include(included_patterns))
